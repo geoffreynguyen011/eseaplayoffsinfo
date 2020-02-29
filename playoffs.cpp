07ee6a896bbc92665cbf1ed23b2b031215826066
Geoffrey Nguyen <geoffrey.nguyen@email.ucr.edu>
2020-02-28 17:38:20 -0800

    Added wins for moveups from open to intermediate, main, and advanced

--- playoffs.cpp
+++ playoffs.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
-
+//int declarations
+//values need to be updated to be more accurate, hard-coded for now
 int openWinsPlayoffs = 10;
 int openWinsNoPlayoffs = 8;
 int imWinsPlayoffs = 11;
@@ -19,6 +20,11 @@ int numMainTeamsPlayoffs, numAdvancedTeamsPlayoffs = 32;
 
 int imMoveups = 2;
 int mainMoveupsFromOpen = 5;
+int mainMoveupsFromIm = 2;
+int advancedMoveupsFromOpen = 8;
+int advancedMoveupsFromIm = 6;
+int advancedMoveupsFromMain = 3;
+int mdlMoveupsFromAdvanced = 3;
 
 
 void playoffsOrNot (string division, int wins) {
@@ -26,19 +32,18 @@ void playoffsOrNot (string division, int wins) {
     string made = "Made playoffs.";
     string notMade = "Did not make playoffs.";
     string depends = "Depends on how many rounds lost.";
+    //invalid values
     if (division != "open" && division != "intermediate" && division != "main" && division != "advanced") {
         cout << "Division does not exist!\n";
         return;
     }
-    if (wins < 0 || wins > 16) {
+    if (wins < minWins || wins > maxWins) {
         cout << "Impossible number of wins.\n";
         return;
     }
+    //valid values
     if (division == "open") {
-        if (wins > 16 || wins < 0) {
-            cout << "Impossible amount of wins.\n";
-        }
-        else if (wins >= openWinsPlayoffs) {
+        if (wins >= openWinsPlayoffs) {
             cout << made << '\n';
         }
         else if (wins <= openWinsPlayoffs) {
@@ -50,10 +55,7 @@ void playoffsOrNot (string division, int wins) {
     }
 
     else if (division == "intermediate") {
-        if (wins > 16 || wins < 0) {
-            cout << "Impossible amount of wins.\n";
-        }
-        else if (wins >= imWinsPlayoffs) {
+        if (wins >= imWinsPlayoffs) {
             cout << made << '\n';
         }
         else if (wins <= imWinsNoPlayoffs) {
@@ -65,10 +67,7 @@ void playoffsOrNot (string division, int wins) {
     }
 
     else if (division == "main") {
-        if (wins > 16 || wins < 0) {
-            cout << "Impossible amount of wins.\n";
-        }
-        else if (wins >= mainWinsPlayoffs) {
+        if (wins >= mainWinsPlayoffs) {
             cout << made << '\n';
         }
         else if (wins <= mainWinsNoPlayoffs) {
@@ -79,10 +78,7 @@ void playoffsOrNot (string division, int wins) {
         }
     }
     else if (division == "advanced") {
-        if (wins > 16 || wins < 0) {
-            cout << "Impossible amount of wins.\n";
-        }
-        else if (wins >= advancedWinsPlayoffs) {
+        if (wins >= advancedWinsPlayoffs) {
             cout << made << '\n';
         }
         else if (wins <= advancedWinsNoPlayoffs) {
@@ -101,11 +97,28 @@ void playoffsOrNot (string division, int wins) {
 
 
 void winsForMoveups(string currDivision, string nextDivision) {
+    int numWins = 0;
+    if (currDivision != "open" && currDivision != "intermediate" && currDivision != "main" && currDivision != "advanced") {
+        cout << "Current division does not exist!";
+        return;
+    }
+    if (nextDivision != "intermediate" && nextDivision != "main" && nextDivision != "advanced" && nextDivision != "MDL") {
+        cout << "Desired division does not exist!";
+        return;
+    }
     if (currDivision == "open") {
         if (nextDivision == "intermediate") {
-            
+            numWins = imMoveups;
+        }
+        else if (nextDivision == "main") {
+            numWins = mainMoveupsFromOpen;
+        }
+        else if (nextDivision == "advanced") {
+            numWins = advancedMoveupsFromOpen;
         }
     }
+
+    cout << "Wins needed from " << currDivision << " to " << nextDivision << " is " << numWins << '\n';
 
     return;
 }
@@ -113,13 +126,13 @@ void winsForMoveups(string currDivision, string nextDivision) {
 void introduction() {
     int input;
     int wins;
-    string division;
+    string division, currDivision, desiredDivision;
 
     while (input != 0) {
         cout << "\nWelcome! Press the corresponding number to select each option: " << endl;
         cout << "0. Exit the program." << endl;
         cout << "1. Check if you made playoffs (open, intermediate, main, advanced)." << endl;
-        cout << "2. Check to see how many wins you need to make IM/Main/Advanced." << endl;
+        cout << "2. Check to see how many wins you need to make intermediate/main/advanced." << endl;
         cin >> input;
         if (input == 1) {
             cout << "Enter which division: ";
@@ -127,6 +140,13 @@ void introduction() {
             cout << "Enter how many wins: ";
             cin >> wins;
             playoffsOrNot(division, wins);
+        }
+        else if (input == 2) { 
+            cout << "Enter current division: ";
+            cin >> currDivision;
+            cout << "Enter desired division: ";
+            cin >> desiredDivision;
+            winsForMoveups(currDivision, desiredDivision);
         }
         //inputs 2, 3, etc...
     }
