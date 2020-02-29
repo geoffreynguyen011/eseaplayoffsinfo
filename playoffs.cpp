81eb7716a6b226e8c1978a905e278b24897e0b45
Geoffrey Nguyen <geoffrey.nguyen@email.ucr.edu>
2020-02-28 17:19:47 -0800

    Moved invalid values into playoffsOrNot function from introduction

--- playoffs.cpp
+++ playoffs.cpp
@@ -21,12 +21,19 @@ int imMoveups = 2;
 int mainMoveupsFromOpen = 5;
 
 
-void playoffsOrNot (int wins, string division) {
+void playoffsOrNot (string division, int wins) {
 
     string made = "Made playoffs.";
     string notMade = "Did not make playoffs.";
     string depends = "Depends on how many rounds lost.";
-
+    if (division != "open" && division != "intermediate" && division != "main" && division != "advanced") {
+        cout << "Division does not exist!\n";
+        return;
+    }
+    if (wins < 0 || wins > 16) {
+        cout << "Impossible number of wins.\n";
+        return;
+    }
     if (division == "open") {
         if (wins > 16 || wins < 0) {
             cout << "Impossible amount of wins.\n";
@@ -111,7 +118,7 @@ void introduction() {
     while (input != 0) {
         cout << "\nWelcome! Press the corresponding number to select each option: " << endl;
         cout << "0. Exit the program." << endl;
-        cout << "1. Check if you made playoffs." << endl;
+        cout << "1. Check if you made playoffs (open, intermediate, main, advanced)." << endl;
         cout << "2. Check to see how many wins you need to make IM/Main/Advanced." << endl;
         cin >> input;
         if (input == 1) {
@@ -119,15 +126,7 @@ void introduction() {
             cin >> division;
             cout << "Enter how many wins: ";
             cin >> wins;
-            if (division == "open") {
-                playoffsOrNot(wins, division);
-            }
-            else if (division == "intermediate") {
-                playoffsOrNot(wins, division);
-            }
-            else if (division == "main") {
-                playoffsOrNot(wins, division);
-            }
+            playoffsOrNot(division, wins);
         }
         //inputs 2, 3, etc...
     }
