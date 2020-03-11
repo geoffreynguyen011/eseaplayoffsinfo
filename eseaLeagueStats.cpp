2ef02cf2fe5048a0a925209531bbb36719c6f565
Geoffrey Nguyen <geoffrey.nguyen@email.ucr.edu>
2020-03-10 17:00:46 -0700

    Added statistics for seasons 28 - seasons 32 involving number of teams, number of wins for each division

---
+++ eseaLeagueStats.cpp
@@ -0,0 +1,76 @@
+/*  s32 stats:
+    advanced: 
+    - 56 total
+    - top 16, cutoff 10-6 with 223 RW, 207 RL
+    main: 
+    - 140 total
+    - top 64, cutoff 9-7 with 194 RW, 194 RL
+        top 32, cutoff 10-6 with 222 RW, 177 RL
+    im: 
+    - 233 total
+    - top 64, cutoff 10-6 with 216 RW, 191 RL
+    open: 
+    - 588 total
+    - top 256, cutoff 9-7 with 180 RW, 192 RL
+
+    s31 stats:
+    advanced: 
+    - 51 total
+    - top 16, cutoff 10-6 with 217 RW, 162 RL
+    main: 
+    - 140 total
+    - top 64, cutoff 9-7 with 188 RW, 168 RL
+        top 32, cutoff 10-6 with 221 RW, 177 RL
+    im: 
+    - 245 total
+    - top 64, cutoff 10-6 with 217 RW, 180 RL
+    open: 
+    - 563 total
+    - top 256, cutoff 8-8 with 221 RW, 178 RL
+
+    s30 stats:
+    advanced: 
+    - 47 totalls
+    
+    - top 16, cutoff 10-6 with 207 RW, 211 RL
+    main: 
+    - 146 total
+    - top 64, cutoff 9-7 with 188 RW, 156 RL
+        top 32, cutoff 10-6 with 227 RW, 161 RL
+    im: 
+    - 240 total
+    - top 64, cutoff 10-6 with 220 RW, 183 RL
+    open: 
+    - 644 total
+    - top 256, cutoff 9-7 with 208 RW, 150 RL
+
+    s29 stats:
+    advanced: 
+    - 46 total
+    - top 16, cutoff 9-7 with 212 RW, 206 RL
+    main: 
+    - 125 total
+    - top 32, cutoff 10-6 with 220 RW, 170 RL
+        top 64, cutoff 8-8 with 208 RW, 190 RL
+    im: 
+    - 217 total
+    - top 64, cutoff 10-6 with 214 RW, 150 RL
+    open: 
+    - 569 total
+    - top 256, cutoff 9-7 with 174 RW, 121 RL
+
+    s28 stats:
+    advanced: 
+    - 39 total
+    - top 16, cutoff 8-7 with 207 RW, 210 RL
+    main: 
+    - 109 total
+    - top 32, cutoff 8-7 with 223 RW, 193 RL
+        top 64, cutoff 7-9 with 199 RW, 206 RL
+    im:
+    - 224 total
+    - top 64, cutoff 10-6 with 213 RW, 189 RL
+    open: 
+    - 728 total
+    - cutoff 9-7 with 216 RW, 176 RL
+*/
