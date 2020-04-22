c15419665c88ab67b35d96982464afcc2ca9afe3
geoffreynguyen011 <gnguy011@ucr.edu>
2020-04-22 12:18:35 -0700

    Added header tag

--- Intro.js
+++ Intro.js
@@ -3,10 +3,12 @@ import React from 'react'
 function Intro() {
     return (
         <div>
-            <h1>Welcome to the ESEA Playoffs Information Website!</h1>
-            <h2>This website is made to give a general probability of whether you made playoffs or not.</h2>
-            <h2>All of the information on this website is based off of previous seasons; this is ENTIRELY independent of ESEA.</h2>
-            <u>Current season: season 33</u>
+            <header>
+                <h1>Welcome to the ESEA Playoffs Information Website!</h1>
+                <h1>This website is made to give a general probability of whether you made playoffs or not.</h1>
+                <h2>All of the information on this website is based off of previous seasons; this is ENTIRELY independent of ESEA.</h2>
+                <h3>Current season: season 33</h3>
+            </header>
         </div>
     )
 }
