7ca524723b21d9d6245607f5f1eed648478b35d3
Geoffrey Nguyen <geoffrey.nguyen@email.ucr.edu>
2021-06-21 21:55:36 -0700

    Sorted every team into array, with stats

--- readAndSortData.py
+++ readAndSortData.py
@@ -13,7 +13,7 @@ total_array = []
 
 # gets each team and its stats
 print(Lines[:10])
-for line in Lines[:10]:
+for line in Lines:
     team_name, wins, losses, ties, win_percent, streak, rounds_won, rounds_lost = "", "", "", "", "", "", "", ""
     # start from back, and get each metric
     # print(line)
@@ -31,14 +31,16 @@ for line in Lines[:10]:
         i -= 1
     current_array.insert(0, rounds_won)
 
-
-    
     i -= 1
     while line[i] != " ":
-        pass
+        streak = line[i] + streak
+        i -= 1
+    current_array.insert(0, streak)
     i -= 1
     while line[i] != " ":
-        pass
+        win_percent = line[i] + win_percent
+        i -= 1
+    current_array.insert(0, win_percent)
     i -= 1
     while line[i] != " ":
         ties = line[i] + ties
@@ -55,6 +57,12 @@ for line in Lines[:10]:
         i -= 1
     current_array.insert(0, wins)
     i -= 1
+    while line[i] != "." and i > 1:
+        team_name = line[i] + team_name
+        i -= 1
+    team_name = team_name[1:]
+    current_array.insert(0, team_name)
+    
     print(current_array)
 
     total_array.append(current_array)
